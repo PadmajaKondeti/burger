b45a007399baf9e55fc84c532d4d06065ed133be
pkondeti <padma.kondeti@gmail.com>
2016-08-04 17:41:47 -0400

    added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi burger');
+INSERT INTO burgers (burger_name) VALUES ('Sylvester burger');
+INSERT INTO burgers (burger_name) VALUES ('Chille burger ');
+INSERT INTO burgers (burger_name) VALUES ('Joe burger');
+INSERT INTO burgers (burger_name) VALUES ('Owens burger');
