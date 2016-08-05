5878b2b673d9fc315996a664a3a16e0d71ec0618
pkondeti <padma.kondeti@gmail.com>
2016-08-04 22:21:57 -0400

    working files with logic

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,8 @@
 Here is where you create all the functions that will do the routing for your app, and the logic of each route.
 */
 var express = require('express');
-var bodyParser = require('body_parser');
-var methodOverride = require('method_override');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
@@ -20,7 +20,7 @@ router.get('/burgers', function (req, res) {
 });
 
 router.post('/burgers/create', function (req, res) {
-	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function () {
+	burger.create(['burger_name'], [req.body.burger_name], function () {
 		res.redirect('/burgers');
 	});
 });
@@ -35,12 +35,5 @@ router.put('/burgers/update/:id', function (req, res) {
 	});
 });
 
-router.delete('/burgers/delete/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
-
-	burger.delete(condition, function () {
-		res.redirect('/burgers');
-	});
-});
 
 module.exports = router;
